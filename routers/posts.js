7e01b40e8c823c34694a3f9d24cfb45b76cbf46b
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-03 16:45:22 +0100

    feat: destroy router completed

--- routers/posts.js
+++ routers/posts.js
@@ -6,32 +6,36 @@ const posts = require("../data/posts-data");
 // ROUTERS
 // Router "index"
 router.get("/", (req, res) => {
-    res.send("Leggo tutti i dati")
+    res.send("Leggo tutti i post")
 })
 
 // Router "show"
 router.get("/:id", (req, res) => {
     const postId = req.params.id;
-    res.send("Leggo i dati dell'elemento con id " + postId)
+    res.send("Leggo i dati del post con id " + postId)
 })
 
 // Router "create"
 router.post("/", (req, res) => {
-    res.send("Creo un nuovo elemento")
+    res.send("Creo un nuovo post")
 })
 
 // Router "update"
 router.put("/:id", (req, res) => {
     const postId = req.params.id;
-    res.send("Aggiorno tutti i dati dell'elemento con id " + postId)
+    res.send("Aggiorno tutti i dati del post con id " + postId)
 })
 
 // Router "modify"
 router.patch("/:id", (req, res) => {
     const postId = req.params.id;
-    res.send("Aggiorno solo i dati modificati dell'elemento con id " + postId)
+    res.send("Aggiorno solo i dati modificati del post con id " + postId)
 })
 
-
+// Router "destroy"
+router.delete("/:id", (req, res) => {
+    const postId = req.params.id;
+    res.send("Elimino il post con id " + postId)
+})
 
 module.exports = router;
